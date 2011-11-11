2bb2239c7d6ed581456203e953abc9595718621f
richardsonclay <richardsonclay@richardsonclays-MacBook-Air.local>
2011-11-10 20:49:52 -0500

    added iphone project

---
+++ client/onemaus/onemaus/AppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.h
+//  onemaus
+//
+//  Created by Clayton Richardson on 11/10/11.
+//  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class ViewController;
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) ViewController *viewController;
+
+@end
---
+++ client/onemaus/onemaus/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  onemaus
+//
+//  Created by Clayton Richardson on 11/10/11.
+//  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
