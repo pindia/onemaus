157e569c28839d9dd544d43ef89e04369d78b9ec
Pindi Albert <pindi.albert@gmail.com>
2011-11-10 22:37:26 -0500

    Increase delay to 1 sec

--- server/static/client.js
+++ server/static/client.js
@@ -5,7 +5,7 @@ function reloadScreen(){
 
 $(document).ready(function(){
 	//reloadScreen();
-	setInterval(reloadScreen, 500);
+	setInterval(reloadScreen, 1000);
 	$("#screen").click(function(event){
 		var off = $("#screen").offset()
 		$.get("/click/" + Math.floor(event.pageX - off.left) + "/" + Math.floor(event.pageY - off.top) +  "/1");
